8ced0e7a561daf77f5646d4eec7a1baf43163704
Rhavgar <sidney5mathias@gmail.com>
2020-02-14 13:04:47 -0300

    JS - Idade

---
+++ 2020/0214/idade.js
@@ -0,0 +1,11 @@
+function nascimento()
+{
+  d = prompt("Informe o dia do seu nascimento");
+  m = prompt("Informe o mes do seu nascimento");
+  y = prompt("Informe o ano do seu nascimento");
+
+  nasc = new Date(y, m, d);
+  idade = Math.floor((Date.now() - nasc)/31557600000);
+
+  alert("Sua idade é " + idade);
+}
